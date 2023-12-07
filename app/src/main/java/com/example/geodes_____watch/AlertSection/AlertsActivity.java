e48b008ac04d212c82c735cd05c20d77455bec1d
YuehanKaichi <Dichosojohncarlo@gmail.com>
2023-12-07 10:12:53 +0800

    alert populate

--- app/src/main/java/com/example/geodes_____watch/AlertSection/AlertsActivity.java
+++ app/src/main/java/com/example/geodes_____watch/AlertSection/AlertsActivity.java
@@ -2,45 +2,83 @@ package com.example.geodes_____watch.AlertSection;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.activity.ComponentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.wear.widget.WearableRecyclerView;
-
 import com.example.geodes_____watch.R;
 import com.example.geodes_____watch.AlertSection.alerts_adapter.Adapter;
 import com.example.geodes_____watch.AlertSection.alerts_adapter.DataModel;
-
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AlertsActivity extends ComponentActivity implements Adapter.OnItemClickListener {
+
+    private List<DataModel> dataList;
+    private Adapter adapter;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerts);
 
-        List<DataModel> dataList = new ArrayList<>();
-        dataList.add(new DataModel("School", "Go to School",R.drawable.baseline_calendar_month_24, "Mon, Tue, Wed", R.drawable.baseline_location_on_24, "Alert#1, ALert2, Alter3",true,  R.drawable.entry));
-        dataList.add(new DataModel("School", "Go to School",R.drawable.baseline_calendar_month_24, "Mon, Tue, Wed", R.drawable.baseline_location_on_24, "Alert#1, ALert2, Alter3", true, R.drawable.exit));
-        dataList.add(new DataModel("School", "Go to School",R.drawable.baseline_calendar_month_24, "Mon, Tue, Wed", R.drawable.baseline_location_on_24, "Alert#1, ALert2, Alter3", true, R.drawable.entry));
+        dataList = new ArrayList<>();
+        adapter = new Adapter(dataList, this);
 
-        WearableRecyclerView recyclerViewww = findViewById(R.id.viewerAlerts);
-        recyclerViewww.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        Adapter adapter = new Adapter(dataList, this);
-        adapter.setOnItemClickListener(this); // Set the listener
-        recyclerViewww.setAdapter(adapter);
-        recyclerViewww.setEdgeItemsCenteringEnabled(false);
+        WearableRecyclerView recyclerView = findViewById(R.id.viewerAlerts);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setEdgeItemsCenteringEnabled(false);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        String userEmail = "yow@gmail.com";
+
+// Assuming "users" is the collection name in Firestore
+        CollectionReference usersCollection = firestore.collection("geofencesEntry");
+
+        Query query = usersCollection.whereEqualTo("email", userEmail);
+
+        query.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                dataList.clear(); // Clear existing data
+
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    // Extract data and create DataModel objects
+                    String alertName = document.getString("title");
+                    String notes = document.getString("notes");
+                    // Add other fields as needed
+
+                    // Create a DataModel object and add it to the list
+                    dataList.add(new DataModel(alertName, notes, R.drawable.baseline_calendar_month_24,
+                            "Mon, Tue, Wed", R.drawable.baseline_location_on_24, "Alert#1, Alert2, Alert3", true, R.drawable.entry));
+                }
+
+                // Notify the adapter that the data set has changed
+                adapter.notifyDataSetChanged();
+            } else {
+                // Handle errors
+                Exception exception = task.getException();
+                if (exception != null) {
+                    // Handle the exception
+                }
+            }
+        });
+
     }
-
 
     @Override
     public void onItemClick(DataModel data) {
         Intent intent = new Intent(AlertsActivity.this, ViewAlertAct.class);
+        // You can pass data to the next activity using intent.putExtra if needed
         startActivity(intent);
     }
-
 }
-
