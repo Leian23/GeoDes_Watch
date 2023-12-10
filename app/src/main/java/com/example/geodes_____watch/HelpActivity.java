db0b78f77d5b164ea4c86d90a15de81b849e760c
YuehanKaichi <Dichosojohncarlo@gmail.com>
2023-12-11 02:39:42 +0800

    add help section

---
+++ app/src/main/java/com/example/geodes_____watch/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.example.geodes_____watch;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+public class HelpActivity extends ComponentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+    }
+
+}
