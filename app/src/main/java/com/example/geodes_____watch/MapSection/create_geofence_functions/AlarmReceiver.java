9338d84f6e6c8079913eed9d9dcc21a532ab51f1
YuehanKaichi <Dichosojohncarlo@gmail.com>
2023-12-13 23:27:12 +0800

    Integrate geofence

---
+++ app/src/main/java/com/example/geodes_____watch/MapSection/create_geofence_functions/AlarmReceiver.java
@@ -0,0 +1,123 @@
+package com.example.geodes_____watch.MapSection.create_geofence_functions;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+
+import androidx.preference.PreferenceManager;
+
+import java.io.IOException;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private static MediaPlayer mediaPlayer;
+    private static Vibrator vibrator;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("AlarmReceiver", "Alarm received");
+
+        // Retrieve the alarm ringtone URI from the intent
+        Uri alarmRingtoneUri = intent.getParcelableExtra("ALARM_RINGTONE_URI");
+        Log.d("AlarmReceiver", "Alarm ringtone URI: " + alarmRingtoneUri);
+
+        // Check if the alarmRingtoneUri is null
+        if (alarmRingtoneUri == null) {
+            Log.e("AlarmReceiver", "Alarm ringtone URI is null");
+            return;
+        }
+
+        // Retrieve the user's preference for vibration
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean enableVibration = sharedPreferences.getBoolean("enable_vibration", true);
+        Log.d("AlarmReceiver", "Enable Vibration: " + enableVibration);
+
+        // Retrieve the user's preference for audio output
+        String selectedAudioOutput = sharedPreferences.getString("audio_output", "both_speaker_and_headphones");
+
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            try {
+                mediaPlayer.setDataSource(context, alarmRingtoneUri);
+                mediaPlayer.setLooping(true);
+
+                // Set the audio attributes based on the selected output
+                if (selectedAudioOutput.equals("speaker")) {
+                    mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_ALARM)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build());
+                } else if (selectedAudioOutput.equals("headphones")) {
+                    // Set audio attributes for headphones
+                    // You may need to customize this based on your requirements
+                    mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build());
+                } else if (selectedAudioOutput.equals("both_speaker_and_headphones")) {
+                    // Set audio attributes for both speaker and headphones
+                    // You may need to customize this based on your requirements
+                    mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_ALARM)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build());
+                }
+
+                // Retrieve the user's preference for volume
+                int volume = sharedPreferences.getInt("volume", 50); // Default volume if not set
+                float volumeLevel = volume / 100.0f; // Convert to a float between 0.0 and 1.0
+                mediaPlayer.setVolume(volumeLevel, volumeLevel);
+
+                // Set up asynchronous preparation
+                mediaPlayer.prepareAsync();
+
+                // Set up a callback for when the preparation is completed
+                mediaPlayer.setOnPreparedListener(mp -> {
+                    Log.d("AlarmReceiver", "MediaPlayer setup completed, starting playback");
+                    mp.start();
+
+                    // Vibrate if enabled
+                    if (enableVibration) {
+                        vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+                        if (vibrator != null) {
+                            long[] pattern = {0, 1000, 1000}; // Vibrate for 1 second, wait for 1 second, repeat
+                            vibrator.vibrate(VibrationEffect.createWaveform(pattern, 0));
+
+                        }
+                    }
+                });
+
+            } catch (IOException e) {
+                Log.e("AlarmReceiver", "Error setting up MediaPlayer: " + e.getMessage());
+                releaseMediaPlayer();
+            }
+        }
+    }
+
+
+    static void stopAlarm() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            releaseMediaPlayer();
+        }
+
+        // Stop vibration if enabled
+        if (vibrator != null) {
+            vibrator.cancel();
+        }
+    }
+
+    private static void releaseMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
--- app/src/main/java/com/example/geodes_____watch/MapSection/create_geofence_functions/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/geodes_____watch/MapSection/create_geofence_functions/GeofenceBroadcastReceiver.java
@@ -1,17 +1,28 @@
 package com.example.geodes_____watch.MapSection.create_geofence_functions;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
+import android.os.PowerManager;
 import android.util.Log;
 import android.widget.Toast;
 
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.preference.PreferenceManager;
+
 import com.example.geodes_____watch.R;
+import com.example.geodes_____watch.Settings_section.MyPreferenceFragment;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
@@ -20,44 +31,67 @@ import java.util.List;
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
     private static final String TAG = "GeofenceReceiver";
+
     private static final String CHANNEL_ID = "GeofenceChannel";
     private static final int NOTIFICATION_ID_OUTER = 1;
     private static final int NOTIFICATION_ID_INNER = 2;
+    private static final int ALARM_NOTIFICATION_ID = 3;
+    public static final int DISMISS_NOTIFICATION_ID = 4;
+    private String fenceName;
+    private String alertType;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "This is geofence!", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onReceive: Geofence broadcast received.");
-
-        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
-        if (geofencingEvent == null) {
-            Log.e(TAG, "onReceive: GeofencingEvent is null");
+        // Acquire a wake lock to ensure the device stays awake during processing
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        if (powerManager == null) {
+            Log.e(TAG, "PowerManager is null");
             return;
         }
 
-        if (geofencingEvent.hasError()) {
-            Log.e(TAG, "onReceive: Error receiving geofence event: " + geofencingEvent.getErrorCode());
-            return;
-        }
+        // Update the WakeLock tag with a unique prefix
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "com.example.geodes_mobile:GeofenceWakeLock");
+        wakeLock.acquire();
 
-        List<Geofence> triggeredGeofences = geofencingEvent.getTriggeringGeofences();
-        int transition = geofencingEvent.getGeofenceTransition();
+        try {
+            // Your existing code for handling geofence events
+            Toast.makeText(context, "This is geofence!", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onReceive: Geofence broadcast received.");
 
-        for (Geofence geofence : triggeredGeofences) {
-            Log.d(TAG, "onReceive: Triggered Geofence - ID: " + geofence.getRequestId());
-            String fenceName = intent.getStringExtra("GEOFENCE_NAME");
-            if (fenceName != null) {
-                if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-                    // Entry Events
-                    Log.d(TAG, "onReceive: ENTER from " + fenceName);
-                    handleEntryEvent(context, fenceName, geofence);
-                } else if (transition == Geofence.GEOFENCE_TRANSITION_EXIT) {
-                    // Exit Events
-                    Log.d(TAG, "onReceive: EXIT from " + fenceName);
-                    handleExitEvent(context, fenceName);
-                } else {
-                    Log.d(TAG, "onReceive: Unexpected transition type: " + transition);
+            GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+            if (geofencingEvent == null) {
+                Log.e(TAG, "onReceive: GeofencingEvent is null");
+                return;
+            }
+
+            if (geofencingEvent.hasError()) {
+                Log.e(TAG, "onReceive: Error receiving geofence event: " + geofencingEvent.getErrorCode());
+                return;
+            }
+
+            List<Geofence> triggeredGeofences = geofencingEvent.getTriggeringGeofences();
+            int transition = geofencingEvent.getGeofenceTransition();
+
+            for (Geofence geofence : triggeredGeofences) {
+                Log.d(TAG, "onReceive: Triggered Geofence - ID: " + geofence.getRequestId());
+                fenceName = intent.getStringExtra("GEOFENCE_NAME");
+                if (fenceName != null) {
+                    if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                        // Entry Events
+                        Log.d(TAG, "onReceive: ENTER from " + fenceName);
+                        handleEntryEvent(context, fenceName, geofence);
+                    } else if (transition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                        // Exit Events
+                        Log.d(TAG, "onReceive: EXIT from " + fenceName);
+                        handleExitEvent(context, fenceName);
+                    } else {
+                        Log.d(TAG, "onReceive: Unexpected transition type: " + transition);
+                    }
                 }
+            }
+        } finally {
+            if (wakeLock != null && wakeLock.isHeld()) {
+                wakeLock.release();
             }
         }
     }
@@ -75,6 +109,8 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
         }
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        MyPreferenceFragment preferenceFragment = new MyPreferenceFragment();
+        alertType = preferenceFragment.getAlertTypePreference(context);;
 
         if (value1 > 2000 && value1 <= 4000) {
             // Outer Geofence Notification
@@ -91,6 +127,7 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
             Log.d(TAG, "onReceive: Entered OuterGeofence. You are near your destination.");
             Toast.makeText(context, "You are near on." + geofenceName, Toast.LENGTH_SHORT).show();
+
             // Perform actions for being near the destination
         } else if (value1 <= 2000) {
             // Inner Geofence Notification
@@ -116,6 +153,129 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
         Log.d(TAG, "onReceive: EXIT from " + geofenceName);
         Toast.makeText(context, "Exit event from: " + geofenceName, Toast.LENGTH_SHORT).show();
         // Additional logic for exit, if needed
+        scheduleAlarm(context, geofenceName);
+        showDismissNotificationExit(context);
+    }
+    private void scheduleAlarm(Context context, String geofenceName) {
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.putExtra("GEOFENCE_NAME", geofenceName);
+
+        // Retrieve the selected alarm ringtone URI from preferences
+        Uri selectedAlarmRingtoneUri = getSelectedAlarmRingtoneUri(context);
+        intent.putExtra("ALARM_RINGTONE_URI", selectedAlarmRingtoneUri);
+
+        // Use PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+        int flags = PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_UPDATE_CURRENT;
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, ALARM_NOTIFICATION_ID, intent, flags);
+
+        long futureInMillis = System.currentTimeMillis(); // Trigger immediately
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        if (alarmManager != null) {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, futureInMillis, pendingIntent);
+        }
+    }
+    private void showDismissNotificationOuter(Context context) {
+        // Create a notification channel for Android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Dismiss Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        // Create an intent for the dismiss action
+        Intent dismissIntent = new Intent(context, DismissReceiver.class);
+        dismissIntent.setAction("com.example.geodes_mobile.ACTION_DISMISS_ALARM");
+
+        // Use PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+        int flags = PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT;
+        PendingIntent dismissPendingIntent = PendingIntent.getBroadcast(context, 5, dismissIntent, flags);
+
+        // Build the notification
+        Notification dismissNotification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("You are near on:" + " " + fenceName)
+                .setContentText("Click to dismiss the alert")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSmallIcon(R.drawable.marker_loc)
+                .addAction(R.drawable.marker_loc, "Dismiss", dismissPendingIntent)
+                .build();
+
+        // Show the notification
+        NotificationManagerCompat.from(context).notify(DISMISS_NOTIFICATION_ID, dismissNotification);
+    }
+    private void showDismissNotificationInner(Context context) {
+        // Create a notification channel for Android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Dismiss Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        // Create an intent for the dismiss action
+        Intent dismissIntent = new Intent(context, DismissReceiver.class);
+        dismissIntent.setAction("com.example.geodes_mobile.ACTION_DISMISS_ALARM");
+
+        // Use PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+        int flags = PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT;
+        PendingIntent dismissPendingIntent = PendingIntent.getBroadcast(context, 6, dismissIntent, flags);
+
+        // Build the notification
+        Notification dismissNotification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("You have Arrive on" + " " + fenceName)
+                .setContentText("Click to dismiss the alert")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSmallIcon(R.drawable.marker_loc)
+                .addAction(R.drawable.marker_loc, "Dismiss", dismissPendingIntent)
+                .build();
+
+        // Show the notification
+        NotificationManagerCompat.from(context).notify(DISMISS_NOTIFICATION_ID, dismissNotification);
+    }
+    private void showDismissNotificationExit(Context context) {
+        // Create a notification channel for Android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Dismiss Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        // Create an intent for the dismiss action
+        Intent dismissIntent = new Intent(context, DismissReceiver.class);
+        dismissIntent.setAction("com.example.geodes_mobile.ACTION_DISMISS_ALARM");
+
+        // Use PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+        int flags = PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT;
+        PendingIntent dismissPendingIntent = PendingIntent.getBroadcast(context, 7, dismissIntent, flags);
+
+        // Build the notification
+        Notification dismissNotification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("You have exited:" + " " + fenceName)
+                .setContentText("Click to dismiss the alert")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSmallIcon(R.drawable.marker_loc)
+                .addAction(R.drawable.marker_loc, "Dismiss", dismissPendingIntent)
+                .build();
+
+        // Show the notification
+        NotificationManagerCompat.from(context).notify(DISMISS_NOTIFICATION_ID, dismissNotification);
+    }
+    private Uri getSelectedAlarmRingtoneUri(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String uriString = sharedPreferences.getString("selected_alarm_ringtone_uri", null);
+
+        if (uriString != null) {
+            return Uri.parse(uriString);
+        } else {
+            // If URI is null, use the default ringtone URI
+            Uri defaultRingtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            return defaultRingtoneUri;
+        }
     }
 
 }
